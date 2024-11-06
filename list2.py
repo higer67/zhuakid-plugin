b294ee21dcdaf2ab08f3c3e68a449b95c2dec6dc
higer67 <38996006+higer67@users.noreply.github.com>
2024-11-07 01:14:00 +0800

    添加二猎场kid信息

--- list2.py
+++ list2.py
@@ -1,21 +1,282 @@
 kid_name_list2 = {
-    
+    '1':[
+        '马赛克kid',
+        '玩具球',
+        '国王kid',
+        '无头kid',
+        'k!d',
+        '吐血kid',
+        '矿工kid',
+        'tas player kid',
+        '思考kid',
+        'po kid'
+    ],
+    '2':[
+        '蓝鸟',
+        '小丑kid',
+        '灰尘草莓',
+        '星之卡比kid',
+        '赤红kid',
+        '持枪kid',
+        '吉他手kid',
+        '靖尊kid',
+        '天使kid',
+        '白kid'
+
+    ],
+    '3':[
+        '仙人掌',
+        '墓碑',
+        'seeker',
+        '以撒kid',
+        '妮娜',
+        '妮娜-精灵',
+        'sakkakun kid',
+        'wum kid',
+        '提取器kid',
+        '幽灵涂鸦',
+        'hd kid'
+
+    ],
+    '4':[
+        'tevi',
+        'evil',
+        'neuro',
+        '雾雨魔理沙kid',
+        '博丽灵梦kid',
+        'madeline'
+    ],
+    '5':[
+        'tgdr',
+        '冬天豆kid',
+        'kidhole'
+    ]
 }
 
 kid_data2 = {
     '1':{
+        '1':{
+            'name': '马赛克kid',
+            'description':
+            '这只kid不喜欢真人出镜，于是他要求猎场管理员给他打了一层马赛克'
+        },
+        '2':{
+            'name': '玩具球',
+            'description':
+            'kid们最喜欢的玩具'
+        },
+        '3':{
+            'name': '国王kid',
+            'description':
+            '散发着王霸之气'
+        },
+        '4':{
+            'name': '无头kid',
+            'description':
+            '脸都不要了'
+        },
+        '5':{
+            'name': 'k!d',
+            'description':
+            '海哥debug be like：'
+        },
+        '6':{
+            'name': '吐血kid',
+            'description':
+            '我来给大家爆一个提取器'
+        },
+        '7':{
+            'name': '矿工kid',
+            'description':
+            '“这是石吗？”\n“这不是石！这是我刚挖出来的神金！看吧，可好看了”'
+        },
+        '8':{
+            'name': 'tas player kid',
+            'description':
+            '可恶，这个kid太灵活了，什么方法都对他无效。但是无缝才是唯一真神',
+            'gif': True
+        },
+        '9':{
+            'name': '思考kid',
+            'description':
+            '被抓走的kid们是不是都被卖掉了？如果是的话，那我值多少钱呢？'
+        },
+        '10':{
+            'name': 'po kid',
+            'description':
+            'bgjw',
+            'gif': True
+        }
         
     },
     '2':{
+        '1':{
+            'name': '蓝鸟',
+            'description':
+            '只是一只又会叫又会飞还难抓，并且 另一个你 不帮忙抓的一只鸟罢了（她最坏了）',
+            'gif': True      
+        },
+        '2':{
+            'name': '小丑kid',
+            'description':
+            '哈哈这次肯定出了'   
+        },
+        '3':{
+            'name': '灰尘草莓',
+            'description':
+            '不知道为什么，许多人都把灰尘草莓叫成煤球草莓，可能是因为隔壁的灰尘精灵长得太像煤球了',
+            'gif': True 
+        },
+        '4':{
+            'name': '星之卡比kid',
+            'description':
+            '星之卡比kid最后悔的事情就是吸了kid，由于星之卡比世界不存在save，他现在得一命通关星之卡比'
+        },
+        '5':{
+            'name': '赤红kid',
+            'description':
+            '此kid登场于《I wanna catch pokemon》,大家和赤红kid一起努力，成为宝可梦训练大师吧！！'
+        },
+        '6':{
+            'name': '持枪kid',
+            'description':
+            'kid知道要被抓了后，他选择了反击',
+            'gif': True
+        },
+        '7':{
+            'name': '吉他手kid',
+            'description':
+            'kid正在用电吉他创造出一首伟大的歌曲，当然这个过程是漫长的',
+            'gif': True
+        },
+        '8':{
+            'name': '靖尊kid',
+            'description':
+            '哎我真的是服了，天天被玩家操控，天天死上万次，那些I Wanna作者能不能消停点啊。玩家发灰食物还会被大明星派来的黑粉质疑，然后玩家赖到我头上，我何德何能啊。小问题，下次再写小作文了。'
+        },
+        '9':{
+            'name': '天使kid',
+            'description':
+            '天使的头上其实有一圈只有聪明人才能看见的光环'
+        },
+        '10':{
+            'name': '白kid',
+            'description':
+            '不是每一只白Kid都有超能力'
+        }
 
     },
     '3':{
+        '1':{
+            'name': '仙人掌',
+            'desciption':
+            '外星人你来地球的目的是什么！！'
+        },
+        '2':{
+            'name': '墓碑',
+            'desciption':
+            'R.I.P'
+        },
+        '3':{
+            'name': 'seeker',
+            'desciption':
+            'seeker和新浪微博长得几乎一模一样，现在已经基本没有人知道它的本名了，都叫它新浪。',
+            'gif': True
+        },
+        '4':{
+            'name': '以撒kid',
+            'desciption':
+            '这大概是与他妈接触最多的kid，但他妈比他爹还狠，于是他只好以save主动（三格充能，使用后使kid死后可以在当前房间复活），被砸烂的R键饰品（无限复活kid），和里罗一样脆的情况在地下室生存。'
+        },
+        '5':{
+            'name': '妮娜',
+            'desciption':
+            '哈亚……你也来了吗，这回我会永远陪着你，不会分开了。'
+        },
+        '6':{
+            'name': '妮娜-精灵',
+            'desciption':
+            '哈亚，能和你一起战斗真的很愉快哦，能一路走到这里，真的是、谢谢你呢。'
+        },
+        '7':{
+            'name': 'sakkakun kid',
+            'desciption':
+            'kakkun kid的家人，每天不是被救就是在被救的路上，像极了若干年前的苏轼和苏辙。'
+        },
+        '8':{
+            'name': 'wum kid',
+            'desciption':
+            '在抓kid流行之前人们其实还抓过大量wum，后来就只剩下这种既是wum又是kid的生物了'
+        },
+        '9':{
+            'name': '提取器kid',
+            'desciption':
+            '提取器kid不是kid提取器，提取它的时候不要写错了，无限的kid提取器是不可能的'
+        },
+        '10':{
+            'name': '幽灵涂鸦',
+            'desciption':
+            '请你获得幸福！！'
+        },
+        '11':{
+            'name': 'hd kid',
+            'desciption':
+            '画质提升了！'
+        }
 
     },
     '4':{
-
+        '1':{
+            'name': 'tevi',
+            'description':
+            '原来你不是兔耳娘啊！',
+            'gif': True
+        },
+        '2':{
+            'name': 'evil',
+            'description':
+            '常年看邪牛的人大多目光炯炯有神，非常自信，且智商逐年上升，最后完全成为天才。看邪牛会改善身体结构，预防各种疾病。人一旦开始看邪牛就说明这个人的智慧品行样貌通通都是上等，这辈子都能在任何地方风姿飒爽。常年看邪牛的人具有强烈的社会正义感，对治安稳定有巨大的贡献，保护普通人的生命。',
+            'gif': True
+        },
+        '3':{
+            'name': 'neuro',
+            'description':
+            '爱钱的龟🐢，好涩的狐🦊，白色的牛⚪，黑色的牛⚫，神经的蜂🐝，神经的蜂🐝，神经的蜂🐝，神经的蜂🐝，神经的蜂🐝\n还等什么？赶快加入神经大人伟大的蜂群吧！\nheart❤',
+            'gif': True
+        },
+        '4':{
+            'name': '雾雨魔理沙kid',
+            'description':
+            '雾雨魔理沙kid的偶像是魔理沙，因此，他常常学习魔理沙处事的方法，这套衣服就是他刚从魔理沙家中偷来的'
+        },
+        '5':{
+            'name': '博丽灵梦kid',
+            'description':
+            'Kid捡到了一套废弃的巫女服，他现在正在tho上cos博丽灵梦\n对了，这个Kid刚才被绀珠传L难度暴打过'
+        },
+        '6':{
+            'name': 'madeline',
+            'description':
+            'Madeline从山顶上摘下来的这颗草莓意外的非常有弹性，于是她把这颗草莓当做了弹跳杆使用',
+            'gif': True
+        }
     },
     '5':{
-        
+        '1':{
+            'name': 'tgdr',
+            'description':
+            'tgdr将自己用过的形象miku和chara融合在一起，并在衣服上缝了他的专属代号“ω”，成为他真正的形象。哦，其实他还为其取了个更响亮的名字：ku cha。看样子他真的很喜欢玩拼音梗。'      
+        },
+        '2':{
+            'name': '冬天豆kid',
+            'description':
+            'Season Studios的冬装靓仔版KID，限定出场！'         
+        },
+        '3':{
+            'name': 'kidhole',
+            'description':
+            '这是在kid下海前，我们在他电脑里面发现的最后一张图',
+            'gif': True      
+        }
     }
 }
