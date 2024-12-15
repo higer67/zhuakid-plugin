8d3f45a04af80a75f66afc66d9ff20652d7b5d7e
higer67 <38996006+higer67@users.noreply.github.com>
2024-12-15 21:21:29 +0800

    更改结构

---
+++ plugin/secret.py
@@ -0,0 +1,5 @@
+secret_list = [
+    ['真虚无kid', '这次你真的什么都没抓到.......'],
+    ['please give me your eyes', 'PLEASEGIVEMEYOUREYESPLEASEGIVEMEYOUREYESPLEASEGIVEMEYOUREYESPLEASEGIVEMEYOUREYESPLEASEGIVEMEYOUREYESPLEASEGIVEMEYOUREYES'],
+    ['c2d6h5', '我去，解密领域大神！']
+]
--- secret.py
+++
@@ -1,5 +0,0 @@
-secret_list = [
-    ['真虚无kid', '这次你真的什么都没抓到.......'],
-    ['please give me your eyes', 'PLEASEGIVEMEYOUREYESPLEASEGIVEMEYOUREYESPLEASEGIVEMEYOUREYESPLEASEGIVEMEYOUREYESPLEASEGIVEMEYOUREYESPLEASEGIVEMEYOUREYES'],
-    ['c2d6h5', '我去，解密领域大神！']
-]
